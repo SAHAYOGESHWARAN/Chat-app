cc092233db23d619f6dd28e75b41601e3266fc54
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-14 11:41:17 +0530

    add chat api master

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { register, login } = require('../controllers/userController');
+const { register, login } = require('../controllers/userController'); // Ensure correct import
 const router = express.Router();
 
+// Route for user registration
 router.post('/register', register);
+
+// Route for user login
 router.post('/login', login);
 
 module.exports = router;
