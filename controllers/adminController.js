477e0b9d47428a47d33ac4a0df9e0d36909c7c53
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-14 11:05:17 +0530

    add chart api master

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,23 +1,21 @@
 const Chat = require('../models/chatModel');
 
 exports.updateChat = async (req, res) => {
-  try {
-    const chat = await Chat.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json(chat);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-exports.deleteChat = async (req, res) => {
-  try {
-    await Chat.findByIdAndDelete(req.params.id);
-    res.status(204).json({ message: 'Chat deleted' });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
+    try {
+      const chatId = req.params.id;
+      const updatedData = req.body;
+  
+      const updatedChat = await Chat.findByIdAndUpdate(chatId, updatedData, { new: true });
+  
+      if (!updatedChat) {
+        return res.status(404).json({ error: 'Chat not found' });
+      }
+  
+      res.status(200).json(updatedChat);
+    } catch (error) {
+      res.status(500).json({ error: 'Server error' });
+    }
+  };
 // Update chat by ID
 exports.updateChat = async (req, res) => {
     try {
@@ -52,3 +50,5 @@ exports.deleteChat = async (req, res) => {
       res.status(500).json({ error: 'Server error' });
     }
   };
+
+
