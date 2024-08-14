9ca5e01ab92883696d9fdbfb7cc813df69e983b3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-14 11:36:37 +0530

    Add chat app master

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: 'user' } // 'admin' or 'user'
-});
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const bcrypt = require('bcryptjs');
-  this.password = await bcrypt.hash(this.password, 10);
+// Hash password before saving user
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
   next();
 });
 
